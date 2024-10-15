cd78a46e1df6866b2b23e676376a04b6f896626a
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-10-15 08:55:46 +0100

    Add functions to monitoring and stop the threads when a philosopher die

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,19 +6,11 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 03:47:58 by ansebast          #+#    #+#             */
-/*   Updated: 2024/10/14 17:02:20 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/10/15 08:54:45 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-long	get_current_time(void)
-{
-	struct timeval	time;
-
-	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
-}
 
 int	all_full(t_data *data)
 {
@@ -39,62 +31,6 @@ int	all_full(t_data *data)
 	return (0);
 }
 
-void	sleep_for(int time_in_ms)
-{
-	usleep(time_in_ms * 1000);
-}
-
-void	print_status(t_philosopher *philo, const char *status)
-{
-	pthread_mutex_lock(&philo->data->write_mutex);
-	if (philo->data->all_alive)
-	{
-		printf("%ld %d %s\n", get_current_time() - philo->data->start_time,
-			philo->id, status);
-	}
-	pthread_mutex_unlock(&philo->data->write_mutex);
-}
-
-void	*philosopher_routine(void *arg)
-{
-	t_philosopher	*philo;
-
-	philo = (t_philosopher *)arg;
-	while (philo->data->all_alive)
-	{
-		if (philo->data->number_of_philosophers == 1)
-		{
-			pthread_mutex_lock(philo->left_fork);
-			print_status(philo, "has taken a fork");
-		}
-		if (philo->id % 2 == 0)
-		{
-			pthread_mutex_lock(philo->left_fork);
-			print_status(philo, "has taken a fork");
-			pthread_mutex_lock(philo->right_fork);
-			print_status(philo, "has taken a fork");
-		}
-		else
-		{
-			pthread_mutex_lock(philo->right_fork);
-			print_status(philo, "has taken a fork");
-			pthread_mutex_lock(philo->left_fork);
-			print_status(philo, "has taken a fork");
-		}
-		print_status(philo, "is eating");
-		philo->last_meal = get_current_time();
-		philo->meals_eaten++;
-		sleep_for(philo->data->time_to_eat);
-		pthread_mutex_unlock(philo->right_fork);
-		pthread_mutex_unlock(philo->left_fork);
-		if (philo->meals_eaten == philo->data->meals_required)
-			break;
-		print_status(philo, "is sleeping");
-		sleep_for(philo->data->time_to_sleep);
-		print_status(philo, "is thinking");
-	}
-	return (NULL);
-}
 
 int	init_philos(t_data *data)
 {
@@ -125,12 +61,107 @@ int	init_philos(t_data *data)
 	return (0);
 }
 
+long	get_current_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
+
+void	sleep_for(int time_in_ms)
+{
+	usleep(time_in_ms * 1000);
+}
+
+void	print_status(t_philosopher *philo, const char *status)
+{
+	pthread_mutex_lock(&philo->data->write_mutex);
+	if (philo->data->all_alive)
+	{
+		printf("%ld %d %s\n", get_current_time() - philo->data->start_time,
+			philo->id, status);
+	}
+	pthread_mutex_unlock(&philo->data->write_mutex);
+}
+
+void	*philosopher_routine(void *arg)
+{
+	t_philosopher	*philo;
+
+	philo = (t_philosopher *)arg;
+	while (philo->data->all_alive)
+	{
+		if (philo->id % 2 == 0)
+		{
+			pthread_mutex_lock(philo->left_fork);
+			print_status(philo, "has taken a fork");
+			pthread_mutex_lock(philo->right_fork);
+			print_status(philo, "has taken a fork");
+		}
+		else
+		{
+			pthread_mutex_lock(philo->right_fork);
+			print_status(philo, "has taken a fork");
+			pthread_mutex_lock(philo->left_fork);
+			print_status(philo, "has taken a fork");
+		}
+
+		philo->last_meal = get_current_time();
+		print_status(philo, "is eating");
+		philo->meals_eaten++;
+		if (philo->meals_eaten == philo->data->meals_required)
+			philo->data->all_alive = 0;
+		sleep_for(philo->data->time_to_eat);
+		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
+
+		print_status(philo, "is sleeping");
+		sleep_for(philo->data->time_to_sleep);
+		print_status(philo, "is thinking");
+	}
+	return (NULL);
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_data	*data;
+	int		i;
+
+	data = (t_data *)arg;
+	while (data->all_alive)
+	{
+		i = 0;
+		while (i < data->number_of_philosophers)
+		{
+			pthread_mutex_lock(&data->write_mutex);
+			long time_since_last_meal = get_current_time() - data->philosophers[i].last_meal;
+
+			if (time_since_last_meal >= data->time_to_die + 5)
+			{
+				data->all_alive = 0;
+				printf("%ld %d died\n", get_current_time() - data->start_time, data->philosophers[i].id);
+			}
+			pthread_mutex_unlock(&data->write_mutex);
+			if (!data->all_alive)
+				break;
+			i++;
+		}
+	}
+	return (NULL);
+}
+
 int	simulation(t_data *data)
 {
 	int	i;
+	pthread_t	monitor;
 
 	data->start_time = get_current_time();
 	data->all_alive = 1;
+
+	if (pthread_create(&monitor, NULL, monitor_routine, (void *)data) != 0)
+		return (1);
+
 	i = 0;
 	while (i < data->number_of_philosophers)
 	{
@@ -139,9 +170,11 @@ int	simulation(t_data *data)
 			return (1);
 		i++;
 	}
+
 	i = 0;
 	while (i < data->number_of_philosophers)
 		pthread_join(data->philosophers[i++].thread, NULL);
+	pthread_join(monitor, NULL);
 	return (0);
 }
 
